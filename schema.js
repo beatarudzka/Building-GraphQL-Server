52c60d7c7a04d1d14dd654f4522267e9b447197e
Beata Rudzka <beatarudzka89@gmail.com>
2020-01-16 21:11:00 +0100

    install express, express-graphql, create an initial dv server create server.js, start the main schema.js

---
+++ schema.js
@@ -0,0 +1,12 @@
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQList,
+  GraphQLNonNull
+} = require('graphql')
+
+module.exports = new GraphQLSchema({
+
+})
